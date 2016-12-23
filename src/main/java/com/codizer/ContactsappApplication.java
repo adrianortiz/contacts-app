6ca0f91deee84a611e8b98231e1aa5f113200be7
Adrian Ortiz Martinez <adrianortizmartinez@gmail.com>
2016-12-22 20:46:09 -0600

    Create project

---
+++ src/main/java/com/codizer/ContactsappApplication.java
@@ -0,0 +1,12 @@
+package com.codizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsappApplication.class, args);
+	}
+}
---
+++ src/test/java/com/codizer/ContactsappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codizer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContactsappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
