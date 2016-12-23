e0598a41368d6ef769b8e34911faaa148ca3f021
Adrian Ortiz Martinez <adrianortizmartinez@gmail.com>
2016-12-22 21:58:49 -0600

    Frontend Login

--- src/main/java/com/codizer/controller/LoginController.java
+++ src/main/java/com/codizer/controller/LoginController.java
@@ -1,11 +1,20 @@
 package com.codizer.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.codizer.model.UserCredential;
 
 @Controller
 public class LoginController {
+	
+	private static final Log LOG = LogFactory.getLog(LoginController.class);
 	
 	@GetMapping("/")
 	public String redirectToLogin() {
@@ -13,12 +22,24 @@ public class LoginController {
 	}
 	
 	@GetMapping("/login")
-	public String showLoginForm() {
-		return "login";
+	public ModelAndView showLoginForm (
+			@RequestParam(name="error", required=false) String error,
+			@RequestParam(name="logout", required=false) String logout) {
+		
+		ModelAndView mav = new ModelAndView("login");
+		mav.addObject("userCredentials", new UserCredential());
+		mav.addObject("error", error);
+		mav.addObject("logout", logout);
+		return mav;
 	}
 	
-	@PostMapping
-	public String loginCheck() {
-		return "contacts";
+	@PostMapping("/logincheck")
+	public String loginCheck(@ModelAttribute(name="userCredentials") UserCredential userCredential) {
+		
+		LOG.info("CALL method: 'loginCheck()' -- Param: '" + userCredential.toString() + "'");
+		if (userCredential.getUsername().equals("user") && userCredential.getPassword().equals("user")) {
+			return "contacts";
+		}
+		return "redirect:/login?error";
 	}
 }
