93a0e0036870497af22ec307efcba7c6c777680d
Adrian Ortiz Martinez <adrianortizmartinez@gmail.com>
2016-12-24 02:59:57 -0600

    Create a Contact (View and Controller).

--- src/main/java/com/codizer/controller/ContactController.java
+++ src/main/java/com/codizer/controller/ContactController.java
@@ -1,18 +1,40 @@
 package com.codizer.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.codizer.contact.ViewConstant;
+import com.codizer.model.ContactModel;
 
 @Controller
-@RequestMapping("/contact")
+@RequestMapping("/contacts")
 public class ContactController {
 	
+	private static final Log LOG = LogFactory.getLog(ContactController.class);
+	
+	@GetMapping("/cancel")
+	public String cancel() {
+		return ViewConstant.CONTACTS;
+	}
+	
 	@GetMapping("/contactform")
-	public String redirectContactForm() {
+	public String redirectContactForm(Model model) {
+		model.addAttribute("contactModel", new ContactModel());
 		return ViewConstant.CONTACT_FORM;
+	}
+	
+	@PostMapping("/addcontact")
+	public String addContact(@ModelAttribute(name="contactModel") ContactModel contactModel,
+			Model model) {
+		LOG.info("METHOD: addContact() -- PARAMS: " + contactModel.toString());
+		model.addAttribute("result", 1);
+		return ViewConstant.CONTACTS;
 	}
 
 }
