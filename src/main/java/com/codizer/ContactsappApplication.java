e504cae9948cc6a3c1f51115e10ce16dd583216c
Adrian Ortiz Martinez <adrianortizmartinez@gmail.com>
2016-12-26 08:19:46 -0600

    Spring Batch
    Creation of a repetitive task

--- src/main/java/com/codizer/ContactsappApplication.java
+++ src/main/java/com/codizer/ContactsappApplication.java
@@ -2,12 +2,14 @@ package com.codizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 // TODO: Auto-generated Javadoc
 /**
  * The Class ContactsappApplication.
  */
 @SpringBootApplication
+@EnableScheduling // Habilitar tarea repetitiva
 public class ContactsappApplication {
 
 	/**
