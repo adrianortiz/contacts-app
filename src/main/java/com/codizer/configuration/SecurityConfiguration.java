70dc424717b8ecba8dea59a57012eb0cd651162a
Adrian Ortiz Martinez <adrianortizmartinez@gmail.com>
2016-12-26 05:23:06 -0600

    Config Spring security

---
+++ src/main/java/com/codizer/configuration/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package com.codizer.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	@Qualifier("userService")
+	private UserDetailsService userService;
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		// Anadir el UserDetailsService que hemos creado
+		// Se agrega tambien un ecriptador de password
+		auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
+		
+	}
+
+	// Metodo a sobreescribir para la configuracion
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// Permitir cargar los recursos estaticos
+		http.authorizeRequests()
+			.antMatchers("/css/*", "/imgs/*").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin().loginPage("/login").loginProcessingUrl("/logincheck")
+			.usernameParameter("username").passwordParameter("password")
+			.defaultSuccessUrl("/loginsucess").permitAll()
+			.and()
+			.logout().logoutUrl("/logout").logoutSuccessUrl("/login?logout").permitAll();
+			
+	}
+
+	
+}
