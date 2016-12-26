c15a2e6787491234c53ef0db37e77ca8c964e05f
Adrian Ortiz Martinez <adrianortizmartinez@gmail.com>
2016-12-26 05:13:01 -0600

    Override UserService method

---
+++ src/main/java/com/codizer/service/impl/UserService.java
@@ -0,0 +1,56 @@
+package com.codizer.service.impl;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.codizer.entity.UserRole;
+import com.codizer.repository.UserRepository;
+
+@Service("userService")
+public class UserService implements UserDetailsService {
+
+	@Autowired
+	@Qualifier("userRepository")
+	private UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		com.codizer.entity.User user = userRepository.findByUsername(username);
+		
+		// Construir la authorities
+		List<GrantedAuthority> authorities = buildAuthorities(user.getUserRole());
+		
+		return buildUser(user, authorities);
+	}
+	
+	// Se encarga de contruir al usuario
+	private User buildUser(com.codizer.entity.User user, List<GrantedAuthority> authorities) {
+		return new User(user.getUsername(), user.getPassword(), user.isEnable(),
+				true, true, true, authorities);
+	}
+	
+	// Transformara nuestro set de usuarios a un listado de GrantedAuthority 
+	// para que Spring sepa los roles de los usarios autentificados
+	private List<GrantedAuthority> buildAuthorities (Set<UserRole> userRoles) {
+		Set<GrantedAuthority> auths = new HashSet<GrantedAuthority>();
+		
+		for(UserRole userRole : userRoles) {
+			auths.add(new SimpleGrantedAuthority(userRole.getRole()));
+		}
+		
+		return new ArrayList<GrantedAuthority>(auths);
+	}
+
+}
